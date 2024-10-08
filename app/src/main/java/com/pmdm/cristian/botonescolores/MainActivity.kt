f24a14f3d01b3cefc3cd748ea70511ba7a031e04
cristian <cregueiromartinez@danielcastelao.org>
2024-10-08 17:06:49 +0200

    lista hecha y arreglado enum

--- app/src/main/java/com/pmdm/cristian/botonescolores/MainActivity.kt
+++ app/src/main/java/com/pmdm/cristian/botonescolores/MainActivity.kt
@@ -31,12 +31,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.android.gms.maps.model.Circle
 import com.pmdm.cristian.botonescolores.ui.theme.BotonesColoresTheme
+import com.pmdm.cristian.botonescolores.Colores
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,9 +53,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun interfazColores(modifier: Modifier = Modifier) {
     var color_texto by remember { mutableStateOf("") }
-    var lista_colores = remember { mutableStateListOf<String>() }
-
+    var lista_colores = remember { mutableStateListOf<Int>() }
+    initialText()
     Column(
+
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -62,119 +65,97 @@ fun interfazColores(modifier: Modifier = Modifier) {
     ) {
 
         Row {
-            Button(onClick = {
-                color_texto = "Rojo"
-                lista_colores.add("Rojo")
-            },
+            Button(
+                onClick = {
+                    color_texto = "Rojo"
+                    lista_colores.add(Colores.ROJO.valorColor)
+                },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Red,
-                )) {
+                )
+            ) {
                 Text(text = "Rojo")
             }
 
-            Button(onClick = {
-                color_texto = "Verde"
-                lista_colores.add("Verde")
-            },
+            Button(
+                onClick = {
+                    color_texto = "Verde"
+                    lista_colores.add(Colores.VERDE.valorColor)
+                },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Green,
-                )) {
+                )
+            ) {
                 Text(text = "Verde")
             }
         }
 
         Row {
-            Button(onClick = {
-                color_texto = "Azul"
-                lista_colores.add("Azul")
-            },
+            Button(
+                onClick = {
+                    color_texto = "Azul"
+                    lista_colores.add(Colores.AZUL.valorColor)
+                },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Blue,
-                )) {
+                )
+            ) {
                 Text(text = "Azul")
             }
 
-            Button(onClick = {
-                color_texto = "Amarillo"
-                lista_colores.add("Amarillo")
-            },
+            Button(
+                onClick = {
+                    color_texto = "Amarillo"
+                    lista_colores.add(Colores.AMARILLO.valorColor)
+                },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Yellow,
                     contentColor = Color.Black
-                )) {
+                )
+            ) {
                 Text(text = "Amarillo")
             }
         }
 
-        Text(text = "Color seleccionado: $color_texto")
+        Text(
 
-        Text(text = "Colores seleccionados:")
+            text = "Color seleccionado: $color_texto",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(top = 30.dp)
+
+        )
+
+        Text(text = "Numeros de los colores seleccionados:",
+            fontStyle = FontStyle.Italic,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(top = 30.dp))
         lista_colores.forEach { color ->
-            Text(text = color)
+            Text(text = color.toString())
         }
     }
 }
 
 @Composable
-fun initialText(){
+fun initialText() {
 
-    Column(verticalArrangement = Arrangement.Center,
+    Column(
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(
-            start = 50.dp,
+            start = 90.dp,
             top = 100.dp
-        )) {
+        )
+    ) {
         Text(
-            text = "ELIGE UN COLOR!",
+            text = "SIMON DICE!",
             fontSize = 30.sp,
             fontWeight = FontWeight.Bold
         )
 
     }
 
-}
-
-@Composable
-fun confirmationColor(name:String){
-
-    Column(verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(
-            top = 80.dp,
-            end = 20.dp
-        )) {
-
-        if(name.equals("Rojo")){
-            Text(
-                text = "Has pulsado el boton $name",
-                fontWeight = FontWeight.Bold,
-                fontSize = 15.sp
-            )
-        }
-        else if (name.equals("Verde")){
-            Text(
-                text = "Has pulsado el boton $name",
-                fontWeight = FontWeight.Bold,
-                fontSize = 15.sp
-
-
-            )
-        }
-        else if (name.equals("Amarillo")){
-            Text(
-                text = "Has pulsado el boton $name",
-                fontWeight = FontWeight.Bold,
-                fontSize = 15.sp
-
-            )
-        }
-        else if (name.equals("Azul")){
-            Text(
-                text = "Has pulsado el boton $name",
-                fontWeight = FontWeight.Bold,
-                fontSize = 15.sp
-
-            )
-        }
-    }
 }
 
