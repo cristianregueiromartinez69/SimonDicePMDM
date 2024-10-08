a9f84ab27c5eac5bb9564bb58e66dbdb9ea09d8b
Cristian <cregueiromartinez@danielcastelao.org>
2024-10-08 14:45:43 +0200

    mejorando interfaz colores

---
+++ app/src/main/java/com/pmdm/cristian/botonescolores/Colores.kt
@@ -0,0 +1,9 @@
+package com.pmdm.cristian.botonescolores
+
+enum class Colores(val valorColor:Int){
+    ROJO(1),
+    VERDE(2),
+    AZUL(3),
+    AMARILLO(4)
+
+}
--- app/src/main/java/com/pmdm/cristian/botonescolores/MainActivity.kt
+++ app/src/main/java/com/pmdm/cristian/botonescolores/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pmdm.cristian.botonescolores
 import android.os.Bundle
 import android.provider.CalendarContract.Colors
 import android.text.style.BackgroundColorSpan
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,6 +24,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -48,71 +50,69 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun interfazColores(modifier: Modifier = Modifier) {
-
     var color_texto by remember { mutableStateOf("") }
+    var lista_colores = remember { mutableStateListOf<String>() }
 
-    initialText()
-    Column (verticalArrangement = Arrangement.Center,
+    Column(
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .padding(70.dp)
-            .padding(top = 150.dp, start = 25.dp)){
+            .padding(top = 150.dp, start = 25.dp)
+    ) {
 
         Row {
-            Button(onClick = {color_texto = "Rojo"
-                             },
+            Button(onClick = {
+                color_texto = "Rojo"
+                lista_colores.add("Rojo")
+            },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Red,
                 )) {
                 Text(text = "Rojo")
-
-
-
-
             }
-            Button(onClick = { color_texto = "Verde"
-                             },
+
+            Button(onClick = {
+                color_texto = "Verde"
+                lista_colores.add("Verde")
+            },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Green,
                 )) {
                 Text(text = "Verde")
-
-
             }
-
         }
-        Row{
-            Button(onClick = { color_texto = "Azul"
-                             },
+
+        Row {
+            Button(onClick = {
+                color_texto = "Azul"
+                lista_colores.add("Azul")
+            },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Blue,
                 )) {
                 Text(text = "Azul")
             }
-            Button(onClick = { color_texto = "Amarillo"
-                             },
+
+            Button(onClick = {
+                color_texto = "Amarillo"
+                lista_colores.add("Amarillo")
+            },
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.Yellow,
                     contentColor = Color.Black
                 )) {
                 Text(text = "Amarillo")
             }
-
         }
 
-        confirmationColor(color_texto)
+        Text(text = "Color seleccionado: $color_texto")
 
-
-
-
-
-
-
-
+        Text(text = "Colores seleccionados:")
+        lista_colores.forEach { color ->
+            Text(text = color)
+        }
     }
-
-
-
 }
 
 @Composable
