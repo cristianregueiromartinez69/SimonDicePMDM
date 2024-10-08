f69aa85d58cd2373ebc55b4e4920664b2434dfad
cristian <cregueiromartinez@danielcastelao.org>
2024-10-08 17:49:50 +0200

    cambios con exito

--- app/src/main/java/com/pmdm/cristian/botonescolores/MainActivity.kt
+++ app/src/main/java/com/pmdm/cristian/botonescolores/MainActivity.kt
@@ -9,13 +9,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Button
@@ -31,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -55,98 +57,105 @@ class MainActivity : ComponentActivity() {
 fun interfazColores(modifier: Modifier = Modifier) {
     var color_texto by remember { mutableStateOf("") }
     var lista_colores = remember { mutableStateListOf<Int>() }
-    var colors by remember { mutableStateOf(Color.White)}
     initialText()
-    Box (modifier = Modifier
-        .fillMaxSize().background(colors)){
-        initialText()
-        Column(
 
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-                .padding(70.dp)
-                .padding(top = 150.dp, start = 25.dp)
-        ) {
+    Column(
 
-            Row {
-                Button(
-                    onClick = {
-                        color_texto = "Rojo"
-                        lista_colores.add(Colores.ROJO.valorColor)
-                        colors = Color.Red
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Red,
-                    )
-                ) {
-                    Text(text = "Rojo")
-                }
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .padding(70.dp)
+            .padding(top = 150.dp, start = 25.dp)
+    ) {
 
-                Button(
-                    onClick = {
-                        color_texto = "Verde"
-                        lista_colores.add(Colores.VERDE.valorColor)
-                        colors = Color.Green
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Green,
-                    )
-                ) {
-                    Text(text = "Verde")
-                }
+        Row {
+            Button(
+                onClick = {
+                    color_texto = "Rojo"
+                    lista_colores.add(Colores.ROJO.valorColor)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red,
+                ),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .padding(3.dp)
+                    .size(70.dp)
+            ) {
+
             }
 
-            Row {
-                Button(
-                    onClick = {
-                        color_texto = "Azul"
-                        lista_colores.add(Colores.AZUL.valorColor)
-                        colors = Color.Blue
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Blue,
-                    )
-                ) {
-                    Text(text = "Azul")
-                }
-
-                Button(
-                    onClick = {
-                        color_texto = "Amarillo"
-                        lista_colores.add(Colores.AMARILLO.valorColor)
-                        colors = Color.Yellow
-                    },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color.Yellow,
-                        contentColor = Color.Black
-                    )
-                ) {
-                    Text(text = "Amarillo")
-                }
-            }
-
-            Text(
-
-                text = "Color seleccionado: $color_texto",
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
+            Button(
+                onClick = {
+                    color_texto = "Verde"
+                    lista_colores.add(Colores.VERDE.valorColor)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Green,
+                ),
                 modifier = Modifier
-                    .padding(top = 30.dp)
+                    .clip(CircleShape)
+                    .padding(3.dp)
+                    .size(70.dp)
+            ) {
 
-            )
-
-            Text(text = "Numeros de los colores seleccionados:",
-                fontStyle = FontStyle.Italic,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .padding(top = 30.dp))
-            lista_colores.forEach { color ->
-                Text(text = color.toString())
             }
         }
-    }
 
+        Row {
+            Button(
+                onClick = {
+                    color_texto = "Azul"
+                    lista_colores.add(Colores.AZUL.valorColor)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue,
+                ),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .padding(3.dp)
+                    .size(70.dp)
+            ) {
+
+            }
+
+            Button(
+                onClick = {
+                    color_texto = "Amarillo"
+                    lista_colores.add(Colores.AMARILLO.valorColor)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Yellow,
+                    contentColor = Color.Black
+                ),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .padding(3.dp)
+                    .size(70.dp)
+            ) {
+
+            }
+        }
+
+        Text(
+
+            text = "Color seleccionado: $color_texto",
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(top = 30.dp)
+
+        )
+
+        Text(text = "Numeros de los colores seleccionados:",
+            fontStyle = FontStyle.Italic,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(top = 30.dp))
+        lista_colores.forEach { color ->
+            Text(text = color.toString())
+        }
+    }
 }
 
 @Composable
